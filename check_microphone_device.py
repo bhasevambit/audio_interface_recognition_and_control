2cddc1617b05389c80d07cd5d83b443f468fb169
bhasevambit <jammadoibit6123@gmail.com>
2023-07-15 09:57:27 +0900

    Update print messeage

--- check_microphone_device.py
+++ check_microphone_device.py
@@ -5,6 +5,8 @@ pa = pyaudio.PyAudio()
 print("\nDevice count : ", pa.get_device_count(), "\n")
 
 for i in range(pa.get_device_count()):
-    print(pa.get_device_info_by_index(i))
+    # print(pa.get_device_info_by_index(i))
+    dev_info = pa.get_device_info_by_index(i)
+    print("index:", dev_info["index"], " ", dev_info["name"])
 
 print("")
