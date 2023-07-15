61c95d4548770d426800207e046320827685e040
bhasevambit <jammadoibit6123@gmail.com>
2023-07-15 10:29:29 +0900

    Change print only microphone devices

--- check_microphone_device.py
+++ check_microphone_device.py
@@ -2,21 +2,25 @@ import pyaudio
 
 pa = pyaudio.PyAudio()
 
-print("\nDevice count : ", pa.get_device_count(), "\n")
+# print("\nTotal Audio Device count : ", pa.get_device_count(), "\n")
+print("\n=== Microphone Devices ===")
 
 for i in range(pa.get_device_count()):
-    # print(pa.get_device_info_by_index(i))
     dev_info = pa.get_device_info_by_index(i)
-    print(
-        "index:",
-        dev_info["index"],
-        " ",
-        dev_info["name"],
-        "( IN-Ch:",
-        dev_info["maxInputChannels"],
-        "/ OUT-Ch:",
-        dev_info["maxOutputChannels"],
-        ")"
-    )
+
+    # print("maxInputChannels:", dev_info["maxInputChannels"])
+
+    if dev_info["maxInputChannels"] != 0:
+        print(
+            "index:",
+            dev_info["index"],
+            " ",
+            dev_info["name"],
+            "( IN-Ch:",
+            dev_info["maxInputChannels"],
+            "/ OUT-Ch:",
+            dev_info["maxOutputChannels"],
+            ")"
+        )
 
 print("")
