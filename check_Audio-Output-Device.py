de5036c2370b2981bd3f0c52c99caac1b92e87b3
bhasevambit <jammadoibit6123@gmail.com>
2023-07-16 13:35:04 +0900

    Update print message

--- check_Audio-Output-Device.py
+++ check_Audio-Output-Device.py
@@ -2,24 +2,44 @@ import pyaudio
 
 pa = pyaudio.PyAudio()
 
-print("\n=== Audio Output Devices (Speaker) ===")
+print("\n=== Audio Output Devices (Speaker) ===\n")
 
-for i in range(pa.get_device_count()):
-    dev_info = pa.get_device_info_by_index(i)
 
-    # print("maxOutputChannels:", dev_info["maxOutputChannels"])
+for host_index in range(0, pa.get_host_api_count()):  # Host APIで大分類
 
-    if dev_info["maxOutputChannels"] != 0:
-        print(
-            "index:",
-            dev_info["index"],
-            " ",
-            dev_info["name"],
-            "( IN-Ch:",
-            dev_info["maxInputChannels"],
-            "/ OUT-Ch:",
-            dev_info["maxOutputChannels"],
-            ")"
+    host_api_info = pa.get_host_api_info_by_index(host_index)
+    print(
+        "--- Host API :",
+        host_api_info["name"],
+        "[INDEX:",
+        host_api_info["index"],
+        ", Default-device-index:",
+        host_api_info["defaultOutputDevice"],
+        "] ---"
+    )
+
+    for device_index in range(
+            0, pa.get_host_api_info_by_index(host_index)['deviceCount']
+    ):  # Deviceで小分類
+
+        dev_info = pa.get_device_info_by_host_api_device_index(
+            host_index,
+            device_index
         )
+
+        if dev_info["maxOutputChannels"] != 0:
+            print(
+                "index:",
+                dev_info["index"],
+                " ",
+                dev_info["name"],
+                "( IN-Ch:",
+                dev_info["maxInputChannels"],
+                "/ OUT-Ch:",
+                dev_info["maxOutputChannels"],
+                ")"
+            )
+
+    print("")
 
 print("")
